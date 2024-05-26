c470701efb5d58596a7e1be78a10fdc5e2a7b304
Mr.Liu <liuw2285@gmail.com>
2024-05-26 16:37:38 +0800

    获取群聊列表新增通过昵称筛选

--- internal/repository/gorm/contact.go
+++ internal/repository/gorm/contact.go
@@ -23,14 +23,16 @@ func (r *gormRepository) GetContactPersonByUsrName(ctx context.Context, usrName 
 
 func (r *gormRepository) GetContactPersons(ctx context.Context, nickname string, offset int) (result []*ContactPerson, total int64, err error) {
 	result = []*ContactPerson{}
-	tx := r.db.WithContext(ctx).Model(&ContactPerson{}).Count(&total)
+	tx := r.db.WithContext(ctx).Model(&ContactPerson{})
 	if offset > 0 {
 		tx = tx.Limit(10).Offset(offset)
 	}
+	tx = tx.Count(&total)
+
 	if nickname != "" {
 		tx = tx.Where("nickname LIKE ?", "%"+nickname+"%")
 	}
-	err = tx.Find(&result).Error
+	err = tx.Count(&total).Find(&result).Error
 	if err != nil {
 		return
 	}
--- internal/repository/gorm/group.go
+++ internal/repository/gorm/group.go
@@ -12,25 +12,27 @@ func (r *gormRepository) SaveGroupContact(ctx context.Context, content *GroupCon
 	return
 }
 
-func (r *gormRepository) GetGroupContactByUsrName(ctx context.Context, usrName string) (content *GroupContact, err error) {
-	content = &GroupContact{}
-	err = r.db.WithContext(ctx).Where("usr_name = ?", usrName).First(content).Error
+func (r *gormRepository) GetGroupContactByUsrName(ctx context.Context, usrName string) (result *GroupContact, err error) {
+	result = &GroupContact{}
+	err = r.db.WithContext(ctx).Where("usr_name = ?", usrName).First(result).Error
 	if err != nil {
 		return
 	}
 	return
 }
 
-func (r *gormRepository) GetGroupContacts(ctx context.Context, nickname string, offset int) (content []*GroupContact, total int64, err error) {
-	content = []*GroupContact{}
-	tx := r.db.WithContext(ctx).Model(&GroupContact{}).Count(&total)
-	if offset > 0 {
-		tx = tx.Limit(10).Offset(offset)
-	}
+func (r *gormRepository) GetGroupContacts(ctx context.Context, nickname string, offset int) (result []*GroupContact, total int64, err error) {
+	result = []*GroupContact{}
+	tx := r.db.WithContext(ctx).Model(&GroupContact{})
 	if nickname != "" {
 		tx = tx.Where("nickname LIKE ?", "%"+nickname+"%")
 	}
-	err = tx.Find(&content).Error
+	tx = tx.Count(&total)
+
+	if offset > 0 {
+		tx = tx.Limit(10).Offset(offset)
+	}
+	err = tx.Find(&result).Error
 	if err != nil {
 		return
 	}
